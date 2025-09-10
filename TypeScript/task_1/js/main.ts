680b3ee8bc7c45a24739e3acd82965bbfe0ab09d
N4yth <nathan.dupuis31@gmail.com>
2025-09-10 14:17:32 +0200

    feat : create a class with methode

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -18,3 +18,30 @@ interface printTeacherFunction {
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string{
+    return 'Currently working';
+  }
+
+  displayName(): string{
+    return this.firstName;
+  }
+}
